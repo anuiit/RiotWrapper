f451adbeb551efb967b4ad37b0cccc4ceaf6e826
Test1 <Anis.mounsi-onisac@email-gillab-rhenry.fr>
2023-06-14 23:44:08 +0200

    The commit removes the Endpoint.py, rdnm/mainV2.py, and rdnm/newMain.py files, and modifies the setup.py file.

--- rdnm/newMain.py
+++
@@ -1,9 +0,0 @@
-from RiotWrapper import RiotAPI
-
-# Create an API instance for the North America region
-na_api = RiotAPI('your-api-key', 'na1')
-na_summoner = na_api.get_summoner_by_name('some-summoner-name')
-
-# Create another API instance for the Europe region
-eu_api = RiotAPI('your-api-key', 'euw1')
-eu_summoner = eu_api.get_summoner_by_name('some-summoner-name')
