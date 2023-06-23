1303dd0da3b31d6c6c565dea6146adb0845456f0
anuiit <Anis.mounsi-onisac@email-gillab-rhenry.fr>
2023-06-23 02:52:27 +0200

    Small stuff
    - Removed debug flag
    - Removed unecessary variables

--- RiotWrapper.py
+++ RiotWrapper.py
@@ -2,11 +2,9 @@ from Endpoints.summoner_v4 import SummonerApi
 from Endpoints.match_v5 import MatchApi
 
 class RiotWrapper:
-    def __init__(self, api_key, region, debug=False):
-        self.api_key = api_key
-        self.debug = debug
-        self.summoner = SummonerApi(region, api_key, debug)
-        self.match = MatchApi(region, api_key, debug)
+    def __init__(self, api_key, region):
+        self.summoner = SummonerApi(region, api_key)
+        self.match = MatchApi(region, api_key)
 
     # get winrate ?
     # general functions to get stats ?
