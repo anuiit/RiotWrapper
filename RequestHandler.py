3b0d2c522fdacfb0c305521cb3d61b683e7371a3
anuiit <anmounsi01@gmail.com>
2023-07-04 13:06:05 +0200

    refactor(RequestHandler.py):
    - Remove CustomHTTPError and ResponseChecker classes to separate files for better organization and reusability.
    - Rename REGION_TO_PLATFORM variable to DOMAIN_TO_PLATFORM for clarity.
    - Add max_retries parameter to RequestHandler constructor to control the number of retries for failed requests.
    - Remove unused expire_after parameter from RequestHandler constructor.
    - Refactor build method in RequestHandler to improve readability and reduce duplication.
    - Refactor make_request method in RequestHandler to handle different types of exceptions and retry failed requests.
    - Add handle_error method to handle different types of exceptions and provide appropriate logging messages.

--- RequestHandler.py
+++ RequestHandler.py
@@ -19,88 +19,53 @@ REGION_TO_PLATFORM = {
     'oc1': 'americas'
 }
 
-class CustomHTTPError(HTTPError):
-    def __init__(self, response):
-        super().__init__(f'Request to {response.url} failed with status code {response.status_code}. Details: {response.text}')
-
-
-class ResponseChecker:
-    @staticmethod
-    def check(response, max_retries=5):
-        handlers = {
-            200: ResponseChecker.__handle_ok,
-            429: ResponseChecker.__handle_429
-        }
-
-        retries = 0
-        retry_after = 10
-
-        for retries in range(max_retries):
-            handler = handlers.get(response.status_code, CustomHTTPError(response))
-            result = handler(response, retry_after)
-
-            if result or not result:
-                return result
-            else:
-                retries += 1
-                retry_after *= 2
-                ResponseChecker.__wait(retry_after)
-
-        raise CustomHTTPError(response)
-
-    @staticmethod
-    def __handle_ok(response, retry_after):
-        return True
-
-    @staticmethod
-    def __handle_429(response, retry_after):
-        retry_after = int(response.headers.get('Retry-After', retry_after))
-        ResponseChecker.__wait(retry_after)
-        return False
-
-    @staticmethod
-    def __wait(retry_after):
-        logging.info(f'Retrying in {retry_after} seconds...')
-        time.sleep(retry_after + 1) # Just to be safe
-        logging.info(f'Waited {retry_after} seconds.\n')
-    
 
 class RequestHandler:
-    def __init__(self, api_key, region, use_platform, expire_after=3600):
+    def __init__(self, api_key, region, use_platform, expire_after=3600, max_retries=3):
         self.api_key = api_key
         self.region = region
         self.use_platform = use_platform
+        self.expire_after = expire_after
+        self.max_retries = max_retries
         self.session = requests.Session()
         self.set_cache(expire_after)
+
+        self.retries = 0
     
     def set_cache(self, expire_after, cache_name='riot_api_cache'):
         requests_cache.install_cache(cache_name, expire_after=expire_after)
 
     def build(self, region, endpoint, query_params=None):
-        platform = REGION_TO_PLATFORM[region]
-
-        if self.use_platform:
-            base_url = f"https://{platform}.api.riotgames.com"
-        else:
-            base_url = f"https://{region}.api.riotgames.com"
-
-        if query_params:
-            url = base_url + endpoint + '?' + urlencode(query_params)
-        else:
-            url = base_url + endpoint
-
-        return url
+        domain = REGION_TO_PLATFORM[region] if self.use_platform else region
+        base_url = f"https://{domain}.api.riotgames.com{endpoint}"
+        
+        return base_url if not query_params else f'{base_url}?{urlencode(query_params)}'
 
     def make_request(self, endpoint, query_params=None):
         url = self.build(self.region, endpoint, query_params=query_params)
         headers = {'X-Riot-Token': self.api_key}
 
-        while True:
-            response = self.session.get(url, headers=headers)
-            check_result = ResponseChecker.check(response)
+        while self.retries < self.max_retries:
+            try:
+                response = self.session.get(url, headers=headers)
+                response.raise_for_status()
 
-            if check_result:
                 return response.json()
-            elif not check_result:
-                logging.DEBUG(f'\nRetrying after 429 error: {str(response.status_code)}, \nurl : {url}, \nheaders : {headers}, \nresponse : {response.json()}')
-                continue
+            except requests.exceptions.RequestException as e:
+                RequestHandler.handle_error(e)
+                self.retries += 1
+                logging.info(f"Retrying request {self.retries}/{self.max_retries}")
+
+    def handle_error(exception):
+        if isinstance(exception, requests.exceptions.ConnectionError):
+            logging.info("Unable to connect to the server:", str(exception))
+        elif isinstance(exception, requests.exceptions.HTTPError):
+            logging.info("HTTP error occurred:", str(exception))
+
+            if exception.response.status_code == 429:
+                retry_after = exception.response.headers['Retry-After']
+                logging.info(f"Waiting {retry_after} seconds before retrying...")
+                
+                time.sleep(int(retry_after))
+        else:
+            logging.info("An unexpected error occurred:", str(exception))
