6f379917ea60d828506d75609d331d67eced8f45
anuiit <anmounsi01@gmail.com>
2023-07-05 17:06:19 +0200

    chore(RiotWrapper.py): add support for additional Riot API endpoints
    feat(RiotWrapper.py): add support for Champion, ChampionMastery, League, Spectator, LeagueExp, and LolStatus API endpoints
    
    chore(RiotWrapper.py): remove commented out code for future implementation
    
    chore(RiotWrapper.py): add TODO comment for future implementation of API class
    
    chore(RiotWrapper.py): add comment for potential future functionality to change champion id to champion name
    - Added TODO comments for adding Api class and initializing self.api
    - Added bullet points for possible future changes relating to winrate, general stats, games played, stats by champion and role, average stats, and random games
    - Added bullet point changing champion id to champion name

--- RiotWrapper.py
+++ RiotWrapper.py
@@ -1,11 +1,27 @@
 from Endpoints.summoner_v4 import SummonerApi
 from Endpoints.match_v5 import MatchApi
+from Endpoints.champion_v3 import ChampionApi
+from Endpoints.championMastery_v4 import ChampionMasteryApi
+from Endpoints.league_v4 import LeagueApi
+from Endpoints.spectator_v4 import SpectatorApi
+from Endpoints.leagueExp_v4 import LeagueExpApi
+from Endpoints.lolStatus_v4 import LolStatusApi
 
 class RiotWrapper:
     def __init__(self, api_key, region):
+        # TODO: add api class
+        # self.api = Api(region, api_key)
+        # self.summoner = SummonerApi()
+
         self.summoner = SummonerApi(region, api_key)
         self.match = MatchApi(region, api_key)
-
+        self.champion = ChampionApi(region, api_key)
+        self.mastery = ChampionMasteryApi(region, api_key)
+        self.league = LeagueApi(region, api_key)
+        self.spectator = SpectatorApi(region, api_key)
+        self.league_exp = LeagueExpApi(region, api_key)
+        self.lol_status = LolStatusApi(region, api_key)
+        
     # get winrate ?
     # general functions to get stats ?
     # get games played with count ?
@@ -13,3 +29,4 @@ class RiotWrapper:
     # stats by role ?
     # average stats ?
     # random games picked ?
+    # change champion id to champion name ?
